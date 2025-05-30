3cf353a12a5c702e71283565f5ea27e3c4dc9a5e
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2025-05-30 18:44:50 +0400

    setup email ready notification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (User $user, string $token) {
+        return 'https://renter.collectivebaseph.art/reset-password?token='.$token;
+         });
     }
 }
